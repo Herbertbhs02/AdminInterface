2638eda036e8c3df0129703b1d2804693ef1e2b6
Herbert Ssevume <you@example.com>
2019-02-09 12:47:47 +0000

    third

--- src/App.js
+++ src/App.js
@@ -1,111 +1,85 @@
 import React, { Component } from 'react';
 import Data from './Data';
-import Filter from './Filter'
+//import Filter from './Filter'
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-   this.state={  
-		"Shopping-App" : {  
-			"appId" : "Shopping-App" ,
-			"activeOrigins" : [  
-				{  
-					"id" : "shopping-01" ,
-					"host" : "somehost:80"
-				} ,
-				{  
-					"id" : "shopping-04" ,
-					"host" : "xyzxy:83"
-				}
-			] ,
-			"inactiveOrigins" : [  
-				{  
-					"id" : "shopping-02" ,
-					"host" : "otherhost:81"
-				}
-			] ,
-			"disabledOrigins" : [  
-				{  
-					"id" : "shopping-03" ,
-					"host" : "nnn-nnn:82"
-				}
-			]
-		} ,
-		"Landing-App" : {  
-			"appId" : "Landing-App" ,
-			"activeOrigins" : [  
-				{  
-					"id" : "landing-01" ,
-					"host" : "somehost:80"
-				} ,
-				{  
-					"id" : "landing-02" ,
-					"host" : "xyzxy:83"
-				}
-			] ,
-			"inactiveOrigins" : [  
-				{  
-					"id" : "landing-0N" ,
-					"host" : "otherhost:81"
-				}
-			] ,
-			"disabledOrigins" : [  
-	
-			]
-		},
- filter:''	}
+		this.state={ response:{
+      "shopping" : {
+        "appId" : "shopping",
+        "activeOrigins" : [ {
+          "id" : "shopping-01xx",
+          "host" : "origins-server:8083"
+        }, {
+          "id" : "shopping-02",
+          "host" : "origins-server:8084"
+        }, {
+          "id" : "shopping-03",
+          "host" : "origins-server:8085"
+        }, {
+          "id" : "shopping-04",
+          "host" : "origins-server:8086"
+        }, {
+          "id" : "shopping-05",
+          "host" : "origins-server:8087"
+        } ],
+        "inactiveOrigins" : [ ],
+        "disabledOrigins" : [ ]
+      },
+      "landing" : {
+        "appId" : "landing",
+        "activeOrigins" : [ {
+          "id" : "landing-01",
+          "host" : "origins-server:8081"
+        }, {
+          "id" : "landing-02yy",
+          "host" : "origins-server:8082"
+        } ],
+        "inactiveOrigins" : [ ],
+        "disabledOrigins" : [ ]
+      },
+      "api-server" : {
+        "appId" : "api-server",
+        "activeOrigins" : [ {
+          "id" : "api-server-01",
+          "host" : "origins-server:8088"
+        } ],
+        "inactiveOrigins" : [ ],
+        "disabledOrigins" : [ ]
+      }
+    },
+									current:['shopping','landing'],
+									
+                 }
 
   }
-    origins = (e)=>{this.setState({filter:e})}
-	
 
+   componentDidMount() {
+    var url ='http://styxdemo.greyfire.co.uk:9000/admin/origins/status?pretty'
+    
+    fetch(url)
+    .then(response => response.json())
+     .then((data)=>{ this.setState({response:data,current: Object.keys(data),len:this.state.current.length})})  }   
+	  
+	
+   // origins = (e)=>{this.setState({filter:e})}
+    // Active={this.state['Shopping-App'].activeOrigins} Inactive={this.state['Shopping-App'].inactiveOrigins} app={this.state['Shopping-App'].appId} filter={this.state.filter}
+    // Active={this.state['Landing-App'].activeOrigins} Inactive={this.state['Landing-App'].inactiveOrigins} app={this.state['Landing-App'].appId} filter= {this.state.filter}
+   // const activeMap = Object.values(props.Active).map((app)=><div>{app.activeOrigins[0].id}{app.activeOrigins[0].host} {app.activeOrigins.length}</div>)
+ // const map = Object.keys(this.state.response).map((app)=><div>{app.activeOrigins[0].id}{app.activeOrigins[0].host} {app.activeOrigins.length}</div>)
+  //  <Data Active={this.state.response.shopping.activeOrigins} Inactive={this.state.response.shopping.inactiveOrigins} app={this.state.response.shopping.appId} />
+// <Filter origins={this.origins}/>
   render() {
-	  if(this.state.filter==''){
+    
+	 
     return (
       <div className="App">
-		   <div className='header'><h2>Styx Origins Dashboard</h2></div>
-		   <Filter origins={this.origins}/>
-           <Data Active={this.state['Shopping-App'].activeOrigins} Inactive={this.state['Shopping-App'].inactiveOrigins} app={this.state['Shopping-App'].appId} filter={this.state.filter}/> 
-		   <Data Active={this.state['Landing-App'].activeOrigins} Inactive={this.state['Landing-App'].inactiveOrigins} app={this.state['Landing-App'].appId} filter= {this.state.filter}/>
-		 
-	  </div>
-	
-	);
-	}if(this.state.filter=='s'||this.state.filter=='sh'){
-		return (
-			<div className="App">
-				 <div className='header'><h2>Styx Origins Dashboard</h2></div>
-				 <Filter origins={this.origins}/>
-				 <Data Active={this.state['Shopping-App'].activeOrigins} Inactive={this.state['Shopping-App'].inactiveOrigins} app={this.state['Shopping-App'].appId} filter={this.state.filter}/> 
-				 
-			   
-			</div>
-		  
-		  );
-	}if(this.state.filter=='l'||this.state.filter=='la'){
-		return (
-			<div className="App">
-				 <div className='header'><h2>Styx Origins Dashboard</h2></div>
-				 <Filter origins={this.origins}/>
-				 <Data Active={this.state['Landing-App'].activeOrigins} Inactive={this.state['Landing-App'].inactiveOrigins} app={this.state['Landing-App'].appId} filter= {this.state.filter}/>
-			   
-			</div>
-		  
-		  );
-	}else{return (
-		<div className="App">
-			 <div className='header'><h2>Styx Origins Dashboard</h2></div>
-			 <Filter origins={this.origins}/>
-			
-		   
-		</div>
-	  
-	  );}
-
-  }
+		    <div className='header'><h2>Styx Origins Dashboard</h2></div>
+			      <Data num={this.state.response} current={this.state.current} size ={this.state.current.length}/>
+		  </div>
+	  );
+		} 
 }
-
-
-
 export default App;
--- src/Data.js
+++ src/Data.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import './Data.css';
 const Data = (props) =>{
-    
-	
-	   // const Active = props.Active.map((origins)=><div><span className='map'>{origins.id}</span><span className='map'>{origins.host}</span> <span className='map'>Active</span> <button>Disable</button></div>)
- 
-  const activeMap = props.Active.map((origins)=><table ><tr className='active'><td>{origins.id}</td><td>{origins.host}</td><td>Active</td> <button>Disable</button></tr></table>)
- const inactiveMap = props.Inactive.map((origins)=><table><tr className='inactive'><td>{origins.id}</td><td>{origins.host}</td><td>Inactive</td> <button>Enable</button></tr></table>)
- 
-  
+  //var i = props.size;
+ console.log(props.size)
+ var b=[]
+ var c=[]
+ var h=[]
+ for(let i=0;i<props.size;i++){
+ var Active = props.num[props.current[i]].activeOrigins
+ var Inactive = props.num[props.current[i]].inactiveOrigins
+  h[i]=props.current[i]
+  var activeMap =Active.map((origins)=><table ><tr className='active'><td>{origins.id}</td><td>{origins.host}</td><td>Active</td> <button>Disable</button></tr></table>) 
+  var inactiveMap = Inactive.map((origins)=><table><tr className='inactive'><td>{origins.id}</td><td>{origins.host}</td><td>Inactive</td> <button>Enable</button></tr></table>)
+ b[i]=activeMap; c[i]=inactiveMap }
+      
+ // Active={this.state['Shopping-App'].activeOrigins}
  // const combined = (props.filter=='')?<div>{ActiveMap}{InactiveMap}</div>:(props.filter=='ac')?<div>{ActiveMap}</div>:(props.filter=='in')?<div>{InactiveMap}</div>:<div>Enter more charactor</div>
 
+ 
 
-	
     return (
         
     <div className='data'>
-    <fieldset>
-          <legend>{props.app}</legend>
-        {activeMap}
-        {inactiveMap}
-
-      
-    </fieldset>
+      <fieldset>
+          <legend>{h[0]}</legend> 
+          {b[0]}
+          {c[0]}
+      </fieldset>
     
+      <fieldset>
+          <legend>{h[1]}</legend> 
+           {b[1]}
+           {c[1]}
+      </fieldset>
+
+      <fieldset>
+          <legend>{h[2]}</legend> 
+           {b[2]}
+           {c[2]}
+      </fieldset>
+       
     </div>
   )}
   export default Data;
